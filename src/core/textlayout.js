57262b937d54414dcfea3c0f9fa29f5054a6b712
speedplane <mes65@cornell.edu>
2014-12-22 23:22:51 -0500

    Add some final changes to the column selection. The more I consider this, the more of a hack it seems. I think I will be removing it, and we'll just rely on the layout of the PDF.... at least for now.

--- src/core/textlayout.js
+++ src/core/textlayout.js
@@ -72,31 +72,34 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
      *         layout if it's not a column in order to improve selectability.
      **/
     could_be_column : function (d, right, bottom) {
-      if(right.d < 1.25*d.width/d.innerHTML.length) {
+      var right_d = right.x - d.x - d.width;
+      if(right_d < 1.25*d.width/d.str.length) {
           // If the space to the right less than a char length, not a column.
           return false;
       }
-      if(d.width > page_width/2) {
+      if(d.width > this.bounds.width/2) {
           // Can't be true if we're so wide.
           return false;
       }
-      var divr = textDivs[right.j];
-      if(right.d < 1.25*divr.width/divr.innerHTML.length) {
+      if(right.d < 1.25*right.width/right.str.length) {
           // If the space to the right less than a char length, not a column.
           return false;
       }
-      if(divr.width > page_width/2) {
+      if(right.width > this.bounds.width/2) {
           // Can't be true if the right is so wide.
           return false;
       }
       // If the horizontal space between d and divr is much smaller than the
       // vertical space between the next legitimate line.
-      if(bottom.j !== null && right.d < bottom.d &&
-                      bottom.d < d.height) {
-          return false;
+      if(bottom) {
+        var bottom_d = d.y + d.height - bottom.y;
+        if (right_d < bottom_d && bottom_d < d.height) {
+              return false;
+        }
       }
-      // Whitespace should not connect columns and won't matter if it does.
-      if(d.isWhitespace || divr.isWhitespace) {
+      
+      if(d.isWhitespace || right.isWhitespace) {
+        // we shouldn't enter this... we should skip whitespace first.
           return false;
       }
       
@@ -106,11 +109,12 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
     
     could_be_next_line : function (d, bottom) {
       // Return true if bottom could be a line directly underneath d.
-      if(bottom.j === null) {
+      if(bottom === null) {
           return false;
       }
+      var bottom_d = d.y + d.height - bottom.y;
       // They have to be vertically close
-      if(bottom.d > d.height) {
+      if(bottom_d > d.height) {
           return false;
       }
       // They must horizontally encapsulate each other.
@@ -128,6 +132,12 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
       if (isAllWhitespace(o.str)) {
         // Whitespace elements aren't visible, but they're used for copy/paste.
         o.isWhitespace = true;
+      } else {
+        // Line numbers can mess up flow, so we detect and handle them them.
+        var n = Number(o.str.replace(/^\s+|\s+$/g,''));
+        if(n % 1 === 0) {
+          o.asInt = n;
+        }
       }
       var tx = o.transform;
       
@@ -150,13 +160,26 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
         o.x = tx[4] + (fontAscent * Math.sin(angle));
         o.y = tx[5] - (fontAscent * Math.cos(angle));
       }
-      o.id = i; // Used to uniquely identify object.
+      o.vertical = style.vertical;
+      
+      o.id = i;         // Used to uniquely identify object.
+      o.right = null;   // The nearest object to the right.
+      o.bottom = null;  // The nearest object to the left.
+      o.flow = {
+        right : null,   // Nearest non-whitespace text to the right
+        bottom: null,
+        next  : null,
+        prev  : null,
+      };
+          
       this.quadtree.insert(o);
     },
     
     calculateTextFlow: function (bounds, objs, styles) {
       var timeSlotManager = new TimeSlotManager();
       var self = this;
+      var calc_reflow = true;
+      self.bounds = bounds;
       
       // Put everything into the quadtree so it's fast to look things up.
       self.quadtree = new QuadTree(bounds, 4, 16);
@@ -165,7 +188,7 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
       }
       
       // The top item has a big impact on the flow, so track it.
-      var top_left = { j : null, d : 1e6 };
+      var top_obj = null;
       // Set each element's padding to run to the nearest right and bottom 
       // element. The padding ensures that text selection works.
       for (i = 0; i < len; i++) {
@@ -173,28 +196,100 @@ var TextLayoutEvaluator = (function TextLayoutEvaluatorClosure() {
         var d_x2 = d.x + d.width;
         var d_y2 = d.y + d.height;
         
-        // Keep track of the top-left most item.
-        if(top_left.j === null || d.y > top_left.d) {
-            top_left.j = i;
-            top_left.d = d.y;
+        // Keep track of the top most item.
+        if(!d.isWhitespace && (top_obj === null || d.y > top_obj.y)) {
+            top_obj = d;
+        }
+        if (!calc_reflow) {
+          // Find the first object to the right.
+          self.quadtree.retrieve_xinc(d.x+d.width,d.y,d.height, function (dr) {
+              if(dr.id !== d.id) {
+                d.right = dr.id;
+                return false;
+              }
+          });
+          // Find the object directly below, subtract the height to move down.
+          self.quadtree.retrieve_ydec(d.x,d.y-d.height,d.width, function (db) {
+              if(db.id !== d.id) {
+                d.bottom = db.id;
+                return false;
+              }
+          });
+        } else {
+          // Iterate to the right.
+          self.quadtree.retrieve_xinc(d.x+d.width,d.y,d.height, function (dr) {
+              if(dr.id !== d.id) {
+                // Find the first item to the right.
+                if(d.right === null) {
+                  d.right = dr.id;
+                }
+                if(dr.isWhitespace !== true) {
+                  // Retrieve the first non whitespace item to the right.
+                  d.flow.right = dr.id;
+                  return false;
+                }
+              }
+          });
+          
+          // Iterate down
+          self.quadtree.retrieve_ydec(d.x,d.y-d.height,d.width, function (db) {
+              if(db.id !== d.id) {
+                if(d.bottom === null) {
+                  d.bottom = db.id;
+                }
+                if(db.isWhitespace !== true) {
+                  d.flow.bottom = db.id;
+                  return false;
+                }
+              }
+          });
+          
+          if(d.flow.right !== null) {
+            objs[d.flow.right].flow.left = d.id;
+          }
+          if(d.flow.bottom !== null) {
+            objs[d.flow.bottom].flow.top = d.id;
+          }
+        }
+      }
+      
+      if(calc_reflow) {
+        // Find the top left-most item.
+        while(top_obj && top_obj.flow.left) {
+          top_obj = objs[top_obj.flow.left];
         }
         
-        // Find the first object to the right.
-        self.quadtree.retrieve_xinc(d.x+d.width,d.y,d.height, function (dr) {
-            if(dr.id !== d.id) {
-              d.right = dr.id;
-              return false;
+        for (i = 0; i < len; i++) {
+          d = objs[i];
+          
+          // Put the divs into a linked list based on their order.
+          if(d.vertical) {
+              // Is there such thing as rows of vertical text? FixMe if so.
+              if(d.flow.buttom) {
+                  d.flow.next = d.flow.buttom;
+              }
+          } else if(d.flow.right) {
+            if(!self.could_be_column(d, objs[d.flow.right], 
+                                      objs[d.flow.bottom])) {
+              // Set next to the next object, which may be whitespace. 
+              // We don't a want the flow to skip the whitespace.
+              d.flow.next = d.right;
+            } else if(d.flow.right === i + 1 || d.right === i + 1) {
+              // Put effort into finding the next line in the flow
+              // it is likely beneath this line or in another column.
+              
             }
-        });
-        // Find the object directly below, subtract the height to move down.
-        self.quadtree.retrieve_ydec(d.x,d.y-d.height,d.width, function (db) {
-            if(db.id !== d.id) {
-              d.below = db.id;
-              return false;
-            }
-        });
+          } else {
+            // No text to the right of this element. Rely on the natural flow.
+          }
+          // Make the reverse linked list.
+          if(d.flow.next) {
+              objs[d.flow.next].flow.prev = d.id;
+          }
+        }
       }
-      return objs;
+      
+      return top_obj ? top_obj.id : null;
     }
   };
   return TextLayoutEvaluator;
